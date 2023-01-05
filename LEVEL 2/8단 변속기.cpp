aa4dd6632c45a3cf5c2ec38bbf1db46900e469c4
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-01-05 20:39:28 +0900

    Update 8단 변속기.cpp

--- LEVEL 2/8단 변속기.cpp
+++ LEVEL 2/8단 변속기.cpp
@@ -1,3 +1,21 @@
 https://gakari.tistory.com/entry/CC-STL-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-string-%ED%95%A8%EC%88%98-%EC%82%AC%EC%9A%A9%EB%B2%95
 
+//strcpy : 문자열복사
+// strlen : 문자열길이
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s(8,' ');
+int main(){
+  for(int i=0; i<8; i++)
+    cin>>s[i];
+  if(s=="12345678")
+    cout<<"ascending";
+  else if(s=="87654321")
+    cout<<"descending";
+  else
+    cout<<"mixed";
+}
