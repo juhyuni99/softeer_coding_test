3d26cbd58f62f0e22e476aff9d12559e103438df
juhyuni99 <102004234+juhyuni99@users.noreply.github.com>
2023-01-06 18:32:11 +0900

    Create 지도자동구축.cpp

---
+++ LEVEL 2/지도자동구축.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int N, answer=2;
+	cin >> N;
+	for (int i = 1; i <= N; i++){
+		answer = answer*2-1;
+	}
+	cout << answer*answer;
+	return 0;
+}
